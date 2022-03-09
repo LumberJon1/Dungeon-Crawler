61ded2a907d3cd2186dc795db7bd5c145d2c254d
Jonathan W <jwilliams12117@gmail.com>
2022-03-08 17:28:47 -0700

    Added functionality to inject character name to HTML page output

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-const fs = ("fs");
+const fs = require("fs");
 const Character = require("./utils/character");
-const logCharacter = require("./utils/character-template");
+const {
+    logCharacter,
+    displayName,
+    displayClass,
+    displayXP,
+    displayAttributes,
+    displayImage,
+    displayHP,
+    displayAC,
+    displaySkills,
+    displayEquipment,
+    displayMoney,
+    loadPage
+} = require("./utils/character-template");
 
 
 let character = new Character();
+//Assign any values we want to the new character
+character.name = "Bilbo Baggins";
+
 logCharacter(character);
+
+
+//Write the file to a character HTML page output (for now, a test page)
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile("./public/character-test.html", fileContent, err => {
+            if (err) {
+                reject (err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: "File created."
+            });
+        });
+    });
+};
+
+writeFile(loadPage(character));
