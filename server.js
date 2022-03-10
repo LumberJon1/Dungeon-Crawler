efbefec5168e0129c0656dbe1d507ecaea22955a
Jonathan W <jwilliams12117@gmail.com>
2022-03-10 08:36:50 -0700

    Created get and post routes for server API and HTML pages

--- server.js
+++ server.js
@@ -17,53 +17,83 @@ app.use(express.static("public"));
 
 //HTML routes...
 //GET routes
-app.get("/", (req, res) => {
-    console.log("Homepage get route");
-    res.sendFile(path.join(__dirname, "./public/utils/character-test.html"));
-});
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
 
-//Catch-all Route
-app.get("*", (req, res) => {
-    console.log("Incorrect route");
-    res.sendFile(path.join(__dirname, "./public/utils/character-test.html"));
-});
 
 //API routes...
 //GET routes...
 
 //GET route for a specific character
-app.get("./api/character/:id", (req, res) => {
+app.get("/api/character/:id", (req, res) => {
     let charID = [req.params.id];
     console.log("Character route for "+charID);
+
+    let charactersArray = [];
+
+    //Get the db.json file
+    fs.readFile(path.join(__dirname, "/db/characters.json"), "utf8", (err, data) => {
+        if (err) {
+            throw err;
+        }
+        if (data) {
+            charactersArray = JSON.parse(data);
+        }
+    });
     
-    let characters = JSON.parse("./db/characters.json");
-    res.json(characters[charID]);
+    res.json(charactersArray[charID]);
 });
 
 //GET route for all characters
-app.get("./api/characters", (req, res) => {
+app.get("/api/characters", (req, res) => {
     console.log("GET route for all characters");
-    
-    let characters = JSON.parse("./db/characters.json");
-    res.json(characters);
+
+    let charactersArray = [];
+
+    //Get the db.json file
+    fs.readFile(path.join(__dirname, "/db/characters.json"), "utf8", (err, data) => {
+        if (err) {
+            throw err;
+        }
+        if (data) {
+            charactersArray = JSON.parse(data);       
+        }
+    });
+    res.json(charactersArray);
 });
 
 //POST routes
 //POST a character to the database
-app.post("./api/characters", (req, res) => {
+app.post("/api/characters", (req, res) => {
     let character = req.body;
-
-    let charactersArray = JSON.parse("./db/characters.json");
-    charactersArray.push(character);
-
-    fs.writeFileSync(path.join(__dirname, "./db/characters.json"), JSON.stringify(charactersArray, null, 4), (err) => {
+    
+    //Get the db.json file
+    fs.readFile(path.join(__dirname, "/db/characters.json"), "utf8", (err, data) => {
         if (err) {
             throw err;
         }
-        console.log("Successfully wrote character to db.json");
+        if (data) {
+            let charactersArray = JSON.parse(data);
+            
+            charactersArray.push(character);
+        }
+    
+    fs.writeFileSync(path.join(__dirname, "/db/characters.json"), JSON.stringify(charactersArray, null, 4), (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("Successfully wrote character to db");
+    });
+
+    res.send("Successfully wrote character to db");
+
     });
 });
 
+//Catch-all Route
+app.get("*", (req, res) => {
+    console.log("Incorrect route");
+    res.sendFile(path.join(__dirname, "/public/character-test.html"));
+});
 
 //Listen
 app.listen(PORT, () => console.log("Listening on port "+PORT));
